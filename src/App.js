01732c4279ee448cdf5d1acce47d22147a555d0d
CodeAgony <yaroslav.bkh@gmail.com>
2019-08-17 12:45:56 +0300

    Add bio section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Navbar from './layout/Navbar';
+import Bio from './layout/Bio';
 
 function App() {
 	return (
 		<div className="App">
 			<Navbar />
-			<h1>App goes here</h1>
+			<Bio />
 		</div>
 	);
 }
---
+++ src/layout/Bio.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Bio = () => {
+	return (
+		<div className="bio">
+			<div className="avatar">avatar here</div>
+			<div className="intro">
+				<h1 className="name">header here</h1>
+				<h3 className="occupation">
+					subheader here, quite a bit of stuff actually
+				</h3>
+			</div>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel sit hic
+				voluptatem eligendi perferendis, inventore est veniam eos? Recusandae,
+				excepturi quo eum dolorem quisquam voluptate ut quia. Dolores, enim
+				nostrum? Vero sapiente iure autem reprehenderit eius quis dolor qui
+				repudiandae doloremque voluptates, est deserunt quas sunt dolore maxime
+				nihil quia cum iusto! Dignissimos aut possimus fuga architecto, quisquam
+				tenetur recusandae. Assumenda doloremque expedita nihil at pariatur
+				animi consectetur, nam asperiores? Fuga quibusdam soluta, aliquam
+				possimus labore, tempore nulla atque enim id est sit placeat inventore.
+			</p>
+		</div>
+	);
+};
+
+export default Bio;
