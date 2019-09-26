82cab02e097aae637f9ba9209a0d9b482849789e
Ярослав Бухта <yarik030213@gmail.com>
2019-09-26 11:57:50 +0300

    Updates

---
+++ precache-manifest.7413e5b546a4d94d88a92420e7c20fec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dd150fa888b93577dd6f64793d8bc2e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e67f501e0c4423d756ce",
+    "url": "/portfolio/static/css/main.3a82764f.chunk.css"
+  },
+  {
+    "revision": "8cf063b8c4ae3c945192",
+    "url": "/portfolio/static/js/2.368c8e64.chunk.js"
+  },
+  {
+    "revision": "e67f501e0c4423d756ce",
+    "url": "/portfolio/static/js/main.9e40e292.chunk.js"
+  },
+  {
+    "revision": "5029282666060c428c88",
+    "url": "/portfolio/static/js/runtime~main.cbff171f.js"
+  }
+]);
